8f8f633a0c7f18cf01dca375ebca653af1e9b762
hazem <53303934+hazem-a1@users.noreply.github.com>
2024-05-05 21:38:21 +0300

    reset function in catch all errors

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,9 +1,29 @@
 "use client";
 
-export default function Error(error: Error) {
+import { useEffect } from "react";
+
+export default function Error({
+    error,
+    reset,
+  }: {
+    error: Error & { digest?: string };
+    reset: () => void;
+  }) {
+    useEffect(() => {
+        console.error(error);
+      }, [error]);
     return (
-        <div className="flex min-h-screen items-center justify-center">
-            <h1 className="text-3xl font-bold">some thing went wrong {error.message}</h1>
+        <div className="flex flex-col min-h-screen items-center justify-center">
+            <h1 className="text-3xl font-bold">some thing went wrong</h1>
+            <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        onClick={
+          // Attempt to recover by trying to re-render the invoices route
+          () => reset()
+        }
+      >
+        Try again
+      </button>
         </div>
     );
 }
