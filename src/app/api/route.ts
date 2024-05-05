de86f851cd6a8fd4137192c98c5cbc4d7dc274ce
hazem <53303934+hazem-a1@users.noreply.github.com>
2024-05-05 21:39:29 +0300

    internal route to hide backend url from client component

---
+++ src/app/api/route.ts
@@ -0,0 +1,18 @@
+export const dynamic = 'force-dynamic' // defaults to auto
+
+import { NextResponse, NextRequest} from 'next/server';
+
+export async function GET(request: NextRequest) {
+    const page = request.nextUrl.searchParams.get('page') || 1;
+    const size = request.nextUrl.searchParams.get('size') || 10;
+    const sort = request.nextUrl.searchParams.get('sort') || "lastupdated";
+    const order = request.nextUrl.searchParams.get('order') || "desc";
+
+    const data = await fetch(`${process.env.API_URL}/posts?page=${page}&size=${size}&sort=${sort}&order=${order}`);
+    const json = await data.json();
+    if (!data.ok) {
+      return NextResponse.json({ error: "Some thing went wrong" }, { status: data.status });
+    }
+    return NextResponse.json(json);
+}
+ 
